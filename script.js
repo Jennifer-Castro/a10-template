4040138b830ec12052ef5884e8f9fb25ce71c9a0
JenniferCastro <jennifer.erc@gmail.com>
2022-07-28 10:45:34 -0400

    Praticas da aula

---
+++ script.js
@@ -0,0 +1,69 @@
+let arrayString = ["jennifer", "jessica", "felipe"]
+let arrayNumber = [7,15,11]
+let arrayVariado = ["jennifer", 7, false]
+let arrayNumeroSozinho = [7]
+let arrayVazio = []
+
+
+
+
+// console.log(arrayString.length)
+
+
+// console.log(arrayString[3])
+
+
+// console.log(arrayVariado.includes("jennifer" && 18))
+
+// console.log(arrayNumeroSozinho)
+
+// console.log(arrayString.length)
+
+
+
+// console.log(arrayVariado.includes("jennifer"))
+// console.log(arrayVariado.includes(7))
+// console.log(arrayVariado.includes(false))
+
+// console.log(arrayNumeroSozinho[0])
+
+// console.log(arrayNumeroSozinho.length)
+// console.log(arrayNumeroSozinho)
+
+let copiaArrayString = arrayString
+// console.log(arrayString)
+// console.log(copiaArrayString)
+
+// copiaArrayString.push("orimar")
+// console.log(copiaArrayString)
+// console.log(arrayString)
+
+
+// let copiaArrayString = arrayString.slice()
+// console.log(copiaArrayString)
+
+// copiaArrayString.push("orimar")
+// console.log("cópia", copiaArrayString)
+// console.log("original", arrayString)
+
+// copiaArrayString.shift()
+// console.log(copiaArrayString)
+
+// arrayNumber.sort()
+// console.log(arrayNumber)
+
+console.log(copiaArrayString)
+copiaArrayString.splice(2,2)
+console.log(copiaArrayString)
+
+let arrayNumber2 = [59,17,38,55]
+console.log(arrayNumber2)
+
+arrayNumber2.sort((a,b)=> a-b)
+console.log(arrayNumber2)
+
+arrayNumber2.reverse()
+console.log(arrayNumber2)
+
+
+
